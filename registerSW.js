5c09460e2f639cfc756b8406a06999c004bdf188
Sigma0007 <sigma0007@example.com>
2026-04-09 12:34:30 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SiteFlow/sw.js', { scope: '/SiteFlow/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
